a1f4a4163da4577151fe171497f536faae6439bb
vector.qiu <vector.qiu@gmail.com>
2026-06-06 13:49:58 +0800

    test(sdk): validate installed package consumption

---
+++ cmake/sdk_consumer_smoke.cmake
@@ -0,0 +1,62 @@
+if(NOT DEFINED XGL_BINARY_DIR)
+    message(FATAL_ERROR "XGL_BINARY_DIR is required")
+endif()
+
+if(NOT DEFINED XGL_SMOKE_DIR)
+    message(FATAL_ERROR "XGL_SMOKE_DIR is required")
+endif()
+
+set(install_prefix "${XGL_SMOKE_DIR}/install")
+set(consumer_src "${XGL_SMOKE_DIR}/consumer")
+set(consumer_build "${XGL_SMOKE_DIR}/consumer-build")
+
+file(REMOVE_RECURSE "${XGL_SMOKE_DIR}")
+file(MAKE_DIRECTORY "${consumer_src}")
+
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" --install "${XGL_BINARY_DIR}" --prefix "${install_prefix}"
+    RESULT_VARIABLE install_result
+)
+
+if(NOT install_result EQUAL 0)
+    message(FATAL_ERROR "xgl install failed with exit code ${install_result}")
+endif()
+
+file(WRITE "${consumer_src}/CMakeLists.txt" [=[
+cmake_minimum_required(VERSION 3.21)
+project(xgl_consumer_smoke C)
+
+find_package(xgl CONFIG REQUIRED)
+
+add_executable(xgl_consumer main.c)
+target_link_libraries(xgl_consumer PRIVATE xgl::xgl)
+]=])
+
+file(WRITE "${consumer_src}/main.c" [=[
+#include <xgl/xgl.h>
+
+int main(void) {
+    xgl_config_t config;
+    xgl_config_get_default(&config);
+    return xgl_version_int() >= 10000U ? 0 : 1;
+}
+]=])
+
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" -S "${consumer_src}" -B "${consumer_build}" -G Ninja
+            "-DCMAKE_PREFIX_PATH=${install_prefix}"
+    RESULT_VARIABLE configure_result
+)
+
+if(NOT configure_result EQUAL 0)
+    message(FATAL_ERROR "xgl consumer configure failed with exit code ${configure_result}")
+endif()
+
+execute_process(
+    COMMAND "${CMAKE_COMMAND}" --build "${consumer_build}"
+    RESULT_VARIABLE build_result
+)
+
+if(NOT build_result EQUAL 0)
+    message(FATAL_ERROR "xgl consumer build failed with exit code ${build_result}")
+endif()
