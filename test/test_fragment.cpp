1a48d1b505b31449dd0330e2e5673c8f2abeacdf
vector.qiu <vector.qiu@gmail.com>
2026-06-07 17:04:08 +0800

    test(fragment): require extension metadata reassembly

--- test/test_fragment.cpp
+++ test/test_fragment.cpp
@@ -6,6 +6,7 @@
 
 #include <gtest/gtest.h>
 #include <xgl/xgl_fragment.h>
+#include <xgl/xgl_wire.h>
 #include <cstring>
 #include <vector>
 
@@ -191,6 +192,110 @@ TEST_F(XglFragmentTest, ReassembleFragments) {
     if (complete_data != nullptr) {
         xgl_fragment_free_data(&manager, complete_data);
     }
+}
+
+TEST_F(XglFragmentTest, ReassembleFragmentsFromFragmentExtensionMetadata) {
+    const std::vector<uint8_t> part0 = {'a', 'b', 'c', 'd'};
+    const std::vector<uint8_t> part1 = {'e', 'f', 'g'};
+    const uint32_t message_id = 0x01020304U;
+    const uint32_t message_len = static_cast<uint32_t>(part0.size() + part1.size());
+
+    uint8_t ext_value[16] = {};
+    size_t ext_value_len = 0;
+    ASSERT_EQ(xgl_wire_encode_fragment_ext_value(ext_value,
+                                                 sizeof(ext_value),
+                                                 message_id,
+                                                 static_cast<uint32_t>(part0.size()),
+                                                 message_len,
+                                                 &ext_value_len),
+              XGL_OK);
+
+    uint32_t decoded_message_id = 0;
+    uint32_t decoded_offset = 0;
+    uint32_t decoded_message_len = 0;
+    ASSERT_EQ(xgl_wire_decode_fragment_ext_value(ext_value,
+                                                 ext_value_len,
+                                                 &decoded_message_id,
+                                                 &decoded_offset,
+                                                 &decoded_message_len),
+              XGL_OK);
+
+    uint8_t* complete_data = nullptr;
+    size_t complete_len = 0;
+    EXPECT_EQ(xgl_fragment_process_ext(&manager,
+                                       0x1234,
+                                       0xAABBCCDDU,
+                                       0x11223344U,
+                                       2,
+                                       decoded_message_id,
+                                       decoded_offset,
+                                       decoded_message_len,
+                                       part1.data(),
+                                       part1.size(),
+                                       &complete_data,
+                                       &complete_len,
+                                       1000),
+              XGL_ERR_BUSY);
+    EXPECT_EQ(xgl_fragment_get_reassembly_count(&manager), 1U);
+
+    EXPECT_EQ(xgl_fragment_process_ext(&manager,
+                                       0x1234,
+                                       0xAABBCCDDU,
+                                       0x11223344U,
+                                       2,
+                                       message_id,
+                                       0,
+                                       message_len,
+                                       part0.data(),
+                                       part0.size(),
+                                       &complete_data,
+                                       &complete_len,
+                                       1001),
+              XGL_OK);
+
+    ASSERT_NE(complete_data, nullptr);
+    ASSERT_EQ(complete_len, message_len);
+    EXPECT_EQ(std::memcmp(complete_data, "abcdefg", message_len), 0);
+    EXPECT_EQ(xgl_fragment_get_reassembly_count(&manager), 0U);
+
+    xgl_fragment_free_data(&manager, complete_data);
+}
+
+TEST_F(XglFragmentTest, FragmentExtensionKeyIncludesSession) {
+    const uint8_t part[] = {'x', 'y'};
+    uint8_t* complete_data = nullptr;
+    size_t complete_len = 0;
+
+    EXPECT_EQ(xgl_fragment_process_ext(&manager,
+                                       0x1234,
+                                       1,
+                                       100,
+                                       2,
+                                       77,
+                                       0,
+                                       4,
+                                       part,
+                                       sizeof(part),
+                                       &complete_data,
+                                       &complete_len,
+                                       1000),
+              XGL_ERR_BUSY);
+    EXPECT_EQ(xgl_fragment_process_ext(&manager,
+                                       0x1234,
+                                       1,
+                                       101,
+                                       2,
+                                       77,
+                                       0,
+                                       4,
+                                       part,
+                                       sizeof(part),
+                                       &complete_data,
+                                       &complete_len,
+                                       1001),
+              XGL_ERR_BUSY);
+
+    EXPECT_EQ(xgl_fragment_get_reassembly_count(&manager), 2U);
 }
 
 TEST_F(XglFragmentTest, RejectsFragmentOffsetThatDoesNotMatchIndexOrder) {
